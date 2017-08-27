d793ba083a9abbe8588c0cad4cd7fa0d5eb0f083
Shirley Wu <shirleyxueyangwu@gmail.com>
2017-08-27 12:07:50 -0700

    Use canvas to render screenshot

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
+import * as d3 from 'd3';
 import logo from './logo.svg';
 import './App.css';
 
+import imageUrl from './images/taytay.png';
+var imageWidth = 848;
+var imageHeight = 437;
+
 class App extends Component {
+
+  componentDidMount() {
+    this.canvas = d3.select(this.refs.canvas);
+    this.ctx = this.refs.canvas.getContext('2d');
+    // set canvas width & height
+    this.refs.canvas.width = imageWidth;
+    this.refs.canvas.height = imageHeight;
+
+    var img = new Image();
+    img.onload = () => {
+      this.ctx.drawImage(img, 0, 0, imageWidth, imageHeight);
+    }
+    img.src = imageUrl;
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <canvas ref='canvas' />
       </div>
     );
   }
