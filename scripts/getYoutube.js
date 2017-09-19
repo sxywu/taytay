7b340598a390ce2059cfe155e8195bf55f415d2d
Shirley Wu <shirleyxueyangwu@gmail.com>
2017-09-18 22:00:03 -0700

    Script to download thumbnail images

--- scripts/getYoutube.js
+++ scripts/getYoutube.js
@@ -1,10 +1,17 @@
 var fs = require('fs');
 var https = require('https');
+var request = require('request');
 
 var key = fs.readFileSync('../key.txt', 'utf-8');
 var videos = fs.readFileSync('../data/mv.json', 'utf-8');
 videos = JSON.parse(videos);
 var videoData = [];
+
+function downloadImage(uri, filename, callback) {
+  request.head(uri, function(err, res, body){
+    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+  });
+}
 
 function getVideoData(video) {
   // if we've gotten data for all videos
@@ -17,7 +24,6 @@ function getVideoData(video) {
 
   var url = 'https://www.googleapis.com/youtube/v3/videos?&key=' +
     key + '&id=' + video['Youtube Id'] + '&part=snippet,statistics';
-  console.log('get video ' + video['Youtube Id']);
 
   https.get(url, res => {
     res.setEncoding('utf8');
@@ -30,6 +36,10 @@ function getVideoData(video) {
       data = JSON.parse(data);
       var {snippet, statistics} = data.items[0];
       videoData.push({snippet, statistics});
+
+      // download thumbnail image
+      downloadImage(snippet.thumbnails.standard.url,
+        '../src/images/' + video['Youtube Id'] + '.jpg', () => console.log('done'));
 
       console.log('finished getting video: ' + video['Youtube Id']);
       // after we get the data, call function for next video
