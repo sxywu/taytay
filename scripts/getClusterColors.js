d2d60676b576af54a551f4e8c421c551d67e16a5
Shirley Wu <shirleyxueyangwu@gmail.com>
2017-10-01 13:52:53 -0700

    Script: save colors for a screenshot

--- scripts/getClusterColors.js
+++ scripts/getClusterColors.js
@@ -4,41 +4,50 @@ const getPixels = require('get-pixels');
 const clusterfck = require('clusterfck');
 
 const videoId = 'VuNIsY6JdUw';
-let videoData = fs.readFileSync(`./data/${videoId}.json`, 'utf-8');
+const videoFile = `./data/${videoId}.json`;
+let videoData = fs.readFileSync(videoFile, 'utf-8');
 videoData = JSON.parse(videoData);
 
-getPixels(`./screenshots/${videoId}/${videoData[0].screenshot}`, (err, pixels) => {
-  if (err) return console.log(err);
+function getColorsForImage(image) {
+  getPixels(`./screenshots/${videoId}/${image.screenshot}`, (err, pixels) => {
+    if (err) return console.log(err);
 
-  const [imageWidth, imageHeight, channels] = pixels.shape.slice();
-  const imageData = pixels.data;
-  const colors = [];
+    const [imageWidth, imageHeight, channels] = pixels.shape.slice();
+    const imageData = pixels.data;
+    const colors = [];
 
-  // get the colors
-  for (let x = 0; x < imageWidth; x += 12) {
-    for (let y = 0; y < imageHeight; y += 12) {
-      let offset = x * 4 + y * 4 * imageWidth;
-      let color = [imageData[offset + 0], imageData[offset + 1], imageData[offset + 2]];
-      if (color.join(',') !== '0,0,0') {
-        colors.push(color);
+    // get the colors
+    for (let x = 0; x < imageWidth; x += 12) {
+      for (let y = 0; y < imageHeight; y += 12) {
+        let offset = x * 4 + y * 4 * imageWidth;
+        let color = [imageData[offset + 0], imageData[offset + 1], imageData[offset + 2]];
+        if (color.join(',') !== '0,0,0') {
+          colors.push(color);
+        }
       }
-    }
-  };
+    };
 
-  var clusters = clusterfck.hcluster(colors, 'euclidean', 'complete', 25);
-  var x = 0;
-  clusters = _.map(clusters, hcluster => {
-    var cluster = leaves(hcluster); // flatten the cluster
-    var clusterSize = cluster.length;
-    var colorAverage = _.map(cluster, 'value');
-    colorAverage = [
-      Math.floor(_.meanBy(colorAverage, color => color[0])),
-      Math.floor(_.meanBy(colorAverage, color => color[1])),
-      Math.floor(_.meanBy(colorAverage, color => color[2])),
-    ];
-    console.log(clusterSize, colorAverage);
+    let clusters = clusterfck.hcluster(colors, 'euclidean', 'complete', 25);
+    let x = 0;
+    clusters = _.map(clusters, hcluster => {
+      var cluster = leaves(hcluster); // flatten the cluster
+      var clusterSize = cluster.length;
+      var colorAverage = _.map(cluster, 'value');
+      colorAverage = [
+        Math.floor(_.meanBy(colorAverage, color => color[0])),
+        Math.floor(_.meanBy(colorAverage, color => color[1])),
+        Math.floor(_.meanBy(colorAverage, color => color[2])),
+      ];
+
+      return {size: clusterSize, color: colorAverage};
+    });
+
+    // after getting cluster size and color, save it to image json
+    image.colors = clusters;
+    // save it to file
+    fs.writeFileSync(videoFile, JSON.stringify(videoData));
   });
-});
+}
 
 // recursively go through cluster and get only the leaves
 function leaves(hcluster) {
@@ -48,3 +57,5 @@ function leaves(hcluster) {
  else
    return leaves(hcluster.left).concat(leaves(hcluster.right));
 }
+
+getColorsForImage(videoData[0])
