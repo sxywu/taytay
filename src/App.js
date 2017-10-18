41758e3f738b5658842a27cc6dece3171e1924ea
Shirley Wu <shirleyxueyangwu@gmail.com>
2017-10-17 19:03:33 -0700

    App.js: newly clustered colors

--- src/App.js
+++ src/App.js
@@ -5,23 +5,28 @@ import chroma from 'chroma-js';
 
 import Cluster from './Cluster';
 import Histogram from './visualizations/Histogram';
+
 import videosData from './data/youtube.json';
+import allColors from './data/allColors.json';
 
 class App extends Component {
   render() {
-    let allColors = [];
+    _.each(allColors, color => {
+      return Object.assign(color, {
+        color: chroma(color.color).hsl(),
+      });
+    });
     const images = _.map(videosData, video => {
       const videoData = require(`./data/${video.id}.json`);
-      const videoImage = videoData[Math.floor(videoData.length / 2)]; // take the middle screenshot
-      const colors = _.chain(videoData).map('colors').flatten()
+      const videoImage = videoData.frames[Math.floor(videoData.frames.length / 2)]; // take the middle screenshot
+      const colors = _.chain(videoData.colors)
         .map(color => {
           return Object.assign(color, {
             color: chroma(color.color).hsl(),
           });
         })
-        .filter(color => color.color[2] > 0.1)
+        // .filter(color => color.color[2] > 0.1)
         .value();
-      allColors = _.union(allColors, colors);
 
       return (
         <div>
@@ -31,7 +36,7 @@ class App extends Component {
         </div>
       );
     });
-    
+
     return (
       <div className="App">
         <Histogram colors={allColors} />
