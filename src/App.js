770ddbb9e0eca580970a65d24e83b3af0b007044
Shirley Wu <shirleyxueyangwu@gmail.com>
2017-10-17 17:37:10 -0700

    Show histogram for each video

--- src/App.js
+++ src/App.js
@@ -4,26 +4,37 @@ import _ from 'lodash';
 import chroma from 'chroma-js';
 
 import Cluster from './Cluster';
+import Histogram from './visualizations/Histogram';
 import videosData from './data/youtube.json';
 
 class App extends Component {
   render() {
+    let allColors = [];
     const images = _.map(videosData, video => {
       const videoData = require(`./data/${video.id}.json`);
       const videoImage = videoData[Math.floor(videoData.length / 2)]; // take the middle screenshot
       const colors = _.chain(videoData).map('colors').flatten()
-        .filter(color => chroma(color.color).hsl()[2] > 0.1)
+        .map(color => {
+          return Object.assign(color, {
+            color: chroma(color.color).hsl(),
+          });
+        })
+        .filter(color => color.color[2] > 0.1)
         .value();
+      allColors = _.union(allColors, colors);
 
       return (
         <div>
+          <h3>{video.snippet.title}</h3>
           <img src={`${process.env.PUBLIC_URL}/images/${video.id}/${videoImage.screenshot}`} />
-          <Cluster colors={colors} />
+          <Histogram colors={colors} />
         </div>
       );
     });
+    
     return (
       <div className="App">
+        <Histogram colors={allColors} />
         {images}
       </div>
     );
