b7c03f6ce2e2790795f794efcb02c1dfc2233fd1
Shirley Wu <shirleyxueyangwu@gmail.com>
2017-10-18 11:19:13 -0700

    Histogram: render hue legend

--- src/visualizations/Histogram.js
+++ src/visualizations/Histogram.js
@@ -18,11 +18,13 @@ class Histogram extends Component {
 
     this.processData(this.props.colors);
     this.renderData();
+    this.renderLegend();
   }
 
   componentDidUpdate() {
     this.processData(this.props.colors);
     this.renderData();
+    this.renderLegend();
   }
 
   processData(colors) {
@@ -76,6 +78,31 @@ class Histogram extends Component {
       this.ctx.clearRect(x - 1, 0, 1, this.props.height);
       this.ctx.clearRect(x + colorWidth - 0.25, 0, this.props.width, this.props.height);
     });
+  }
+
+  renderLegend() {
+    // color legend under chart
+    const y = this.props.height - 1 * margin.bottom + 2;
+    const colorWidth = this.xScale(hueDivider) - this.xScale(0);
+    const colorHeight = 0.75 * colorWidth;
+    _.each(this.groups, group => {
+      const color = chroma(group.hue, 0.75, 0.5, 'hsl');
+      const x = this.xScale(group.hue);
+
+      this.ctx.beginPath();
+      this.ctx.fillStyle = color;
+      this.ctx.rect(x, y, colorWidth, colorHeight);
+      this.ctx.fill();
+
+      this.ctx.clearRect(x - 1, y, 1, colorHeight);
+      this.ctx.clearRect(x + colorWidth - 0.25, y, this.props.width, colorHeight);
+    });
+
+    // draw line under chart
+    this.ctx.beginPath();
+    this.ctx.fillStyle = '#000';
+    this.ctx.fillRect(margin.left, this.props.height - margin.bottom,
+      this.props.width - margin.left - margin.right, 1);
   }
 
   render() {
