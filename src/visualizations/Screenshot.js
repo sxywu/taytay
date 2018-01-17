66d7dde264c38daaafea62cdfbffbc43282e51d9
Shirley Wu <shirleyxueyangwu@gmail.com>
2018-01-16 17:34:29 -0800

    Clean Screenshot

--- src/visualizations/Screenshot.js
+++ src/visualizations/Screenshot.js
@@ -167,17 +167,6 @@ class Screenshot extends Component {
   componentDidUpdate() {
     this.renderImage(this.state.imageData);
   }
-  //
-  // calculateColors() {
-  //   const imageWidth = this.props.width;
-  //   const imageHeight = Math.floor(width * ratio);
-  //   for (let x = 0; x < imageWidth; x += 1) {
-  //     for (let y = 0; y < imageHeight; y += 1) {
-  //       let offset = x * 3 + y * 3 * imageWidth;
-  //
-  //     }
-  //   };
-  // }
 
   renderImage = (imageData) => {
     const {hueRange, satRange, lightRange} = this.props.filters;
@@ -185,66 +174,6 @@ class Screenshot extends Component {
     pixels = _.flattenDeep(pixels);
     this.state.filteredImageData.data.set(pixels);
     this.ctx.putImageData(this.state.filteredImageData, 0, 0);
-    // this.refs.container.appendChild(this.render.getCanvas());
-    // const pixels = this.props.colors;
-    // const imageWidth = this.props.width;
-    // const imageHeight = pixels.length / imageWidth / 3;
-    // const filteredImage = [];
-    // for (let x = 0; x < imageWidth; x += 1) {
-    //   for (let y = 0; y < imageHeight; y += 1) {
-    //     let offset = x * 3 + y * 3 * imageWidth;
-    //     let color = [pixels[offset + 0], pixels[offset + 1], pixels[offset + 2]];
-    //
-    //     const keepColor = FilterData.keepColor(color, hueRange, satRange, lightRange);
-    //
-    //     if (!keepColor) {
-    //       // if not keeping color turn it gray
-    //       color = [color[0], 0, color[2]];
-    //     }
-    //     color = chroma(color[0], color[1], color[2], 'hsl').rgb();
-    //     // offset = x * 4 + y * 4 * imageWidth;
-    //     // filteredImage[offset + 0] = color[0];
-    //     // filteredImage[offset + 1] = color[1];
-    //     // filteredImage[offset + 2] = color[2];
-    //     // filteredImage[offset + 3] = 255;
-    //     filteredImage.push(color[0]);
-    //     filteredImage.push(color[1]);
-    //     filteredImage.push(color[2]);
-    //     filteredImage.push(255);
-    //   }
-    // };
-    //
-    // const imageData = new ImageData(new Uint8ClampedArray(filteredImage), imageWidth, imageHeight);
-    // this.ctx.putImageData(imageData, 0, 0);
-    // img.onload = () => {
-    //   this.ctx.drawImage(img, 0, 0, width, height);
-    //   const imageData = this.ctx.getImageData(0, 0, width, height);
-    //   const pixels = imageData.data;
-    //
-    //   // get the colors
-    //   for (let x = 0; x < width; x += 1) {
-    //     for (let y = 0; y < height; y += 1) {
-    //       let offset = x * 4 + y * 4 * width;
-    //       let color = [pixels[offset + 0], pixels[offset + 1], pixels[offset + 2]];
-    //
-    //       if (color.join(',') !== '0,0,0') {
-    //         color = chroma(color).hsl();
-    //
-    //         const keepColor = FilterData.keepColor(color, hueRange, satRange, lightRange);
-    //         if (!keepColor) {
-    //           // if not keeping color turn it gray
-    //           color = [color[0], 0, color[2]];
-    //         }
-    //         color = chroma(color[0], color[1], color[2], 'hsl').rgb();
-    //         pixels[offset + 0] = color[0];
-    //         pixels[offset + 1] = color[1];
-    //         pixels[offset + 2] = color[2];
-    //       }
-    //     }
-    //   };
-    //
-    //   this.ctx.putImageData(imageData, 0, 0);
-    // }
   }
 
   render() {
